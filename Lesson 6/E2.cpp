c7290cc593e98d825578e4069a6783903670fe3d
ares2347 <88496216+ares2347@users.noreply.github.com>
2022-04-15 20:09:25 +0700

    BT Lab

---
+++ Lesson 6/E2.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+	int n;
+	do {
+	printf("input n:");
+	scanf("%d",&n);
+	}while(n<=0);
+	int a[n];
+	for (int i=0; i<n;i++){
+		printf("input number:");
+		scanf("%d",&a[i]);
+		for(int j=i-1; j>=0; j--){
+			if(a[j]>a[i]){
+				printf("so %d bi nhap sai thu tu, vui long nhap lai: ",a[i]);
+				scanf("%d",&a[i]);
+			}
+		}	
+	}
+	
+	for (int i=0; i<n; i++){
+		printf("%d\t", a[i]);
+	}
+}
